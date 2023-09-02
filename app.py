3f762379f6cab62672fda46b1c5acf4fd90c1c6e
aCarinato <alessandro.carinato@gmail.com>
2023-09-02 11:18:41 +0200

    BUG - url not found

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from bs4 import BeautifulSoup
 import requests
 
 app = Flask(__name__)
-CORS(app)
+# CORS(app)
+CORS(app, origins="*")
 
 
 @app.route("/scrape", methods=["POST"])
@@ -14,6 +15,8 @@ def scrape():
     try:
         website_url = request.json["url"]
         response = requests.get(website_url)
+
+        print(website_url)
 
         # Check the HTTP status code to see if the request was successful
         if response.status_code == 200:
@@ -64,69 +67,69 @@ def scrape():
 
 # @app.route("/scrape", methods=["POST"])
 # def scrape():
-#         # get the URL from the request arguments
-#         # url = request.args.get('url')
+#     # get the URL from the request arguments
+#     # url = request.args.get('url')
 
-#         website_url = request.json["url"]
+#     website_url = request.json["url"]
 
-#         # print(website_url)
+#     # print(website_url)
 
-#         response = requests.get(website_url)
-#         # yc_web_page = response.text
+#     response = requests.get(website_url)
+#     # yc_web_page = response.text
 
-#         # make a request to the URL and get the content
-#         # response = requests.get(url)
-#         soup = BeautifulSoup(response.text, "html.parser")
-#         print(soup)
-#         # soup = BeautifulSoup(response.content, 'html.parser')
+#     # make a request to the URL and get the content
+#     # response = requests.get(url)
+#     soup = BeautifulSoup(response.text, "html.parser")
+#     print(soup)
+#     # soup = BeautifulSoup(response.content, 'html.parser')
 
-#         # define the relevant tags to scrape
-#         relevant_tags = [
-#             "div",
-#             "h1",
-#             "h2",
-#             "h3",
-#             "h4",
-#             "h5",
-#             "h6",
-#             "p",
-#             "a",
-#             "span",
-#             "em",
-#             "strong",
-#             "b",
-#             "i",
-#             "u",
-#             "ol",
-#             "ul",
-#             "li",
-#             "img",
-#             "table",
-#             "thead",
-#             "tbody",
-#             "tfoot",
-#             "tr",
-#             "th",
-#             "td",
-#         ]
+#     # define the relevant tags to scrape
+#     relevant_tags = [
+#         "div",
+#         "h1",
+#         "h2",
+#         "h3",
+#         "h4",
+#         "h5",
+#         "h6",
+#         "p",
+#         "a",
+#         "span",
+#         "em",
+#         "strong",
+#         "b",
+#         "i",
+#         "u",
+#         "ol",
+#         "ul",
+#         "li",
+#         "img",
+#         "table",
+#         "thead",
+#         "tbody",
+#         "tfoot",
+#         "tr",
+#         "th",
+#         "td",
+#     ]
 
-#         text_on_page = ""
-#         for tag in soup.find_all(relevant_tags):
-#             if tag.string:
-#                 text_on_page += tag.string + " "
+#     text_on_page = ""
+#     for tag in soup.find_all(relevant_tags):
+#         if tag.string:
+#             text_on_page += tag.string + " "
 
-#         print(text_on_page)
-#         # print(text_on_page)
-#         # # find all the relevant elements in the page and join them into a single string
-#         # text_on_page = "\n".join([tag.get_text() for tag in soup.find_all(relevant_tags)])
+#     print(text_on_page)
+#     # print(text_on_page)
+#     # # find all the relevant elements in the page and join them into a single string
+#     # text_on_page = "\n".join([tag.get_text() for tag in soup.find_all(relevant_tags)])
 
-#         # response = {
-#         #     'success': True,
-#         #     'content': text_on_page
-#         # }
+#     # response = {
+#     #     'success': True,
+#     #     'content': text_on_page
+#     # }
 
-#         # create a response containing the scraped text
-#         return text_on_page
+#     # create a response containing the scraped text
+#     return text_on_page
 
 
 if __name__ == "__main__":
