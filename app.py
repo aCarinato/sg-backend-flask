a55cb037844144e02016e5d790d57269ab34877e
aCarinato <alessandro.carinato@gmail.com>
2023-09-02 10:41:08 +0200

    url not found

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
+
 # from markupsafe import escape
 from bs4 import BeautifulSoup
 import requests
@@ -7,35 +8,126 @@ import requests
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/scrape', methods=['POST'])
+
+@app.route("/scrape", methods=["POST"])
 def scrape():
-    # get the URL from the request arguments
-    # url = request.args.get('url')
+    try:
+        website_url = request.json["url"]
+        response = requests.get(website_url)
 
-    website_url = request.json['url']
+        # Check the HTTP status code to see if the request was successful
+        if response.status_code == 200:
+            soup = BeautifulSoup(response.text, "html.parser")
 
-    # print(website_url)
+            relevant_tags = [
+                "div",
+                "h1",
+                "h2",
+                "h3",
+                "h4",
+                "h5",
+                "h6",
+                "p",
+                "a",
+                "span",
+                "em",
+                "strong",
+                "b",
+                "i",
+                "u",
+                "ol",
+                "ul",
+                "li",
+                "img",
+                "table",
+                "thead",
+                "tbody",
+                "tfoot",
+                "tr",
+                "th",
+                "td",
+            ]
 
-    response = requests.get(website_url)
-    # make a request to the URL and get the content
-    # response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    # soup = BeautifulSoup(response.content, 'html.parser')
+            text_on_page = ""
+            for tag in soup.find_all(relevant_tags):
+                if tag.string:
+                    text_on_page += tag.string + " "
 
-    # define the relevant tags to scrape
-    relevant_tags = ['div', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'span', 'em', 'strong', 'b', 'i', 'u', 'ol', 'ul', 'li', 'img', 'table', 'thead', 'tbody', 'tfoot', 'tr', 'th', 'td']
+            return jsonify({"success": True, "content": text_on_page})
 
-    text_on_page = ''
-    for tag in soup.find_all(relevant_tags):
-        if tag.string:
-            text_on_page += tag.string + ' '
+        else:
+            return jsonify({"success": False, "error": "URL not found or inaccessible"})
 
-    # print(text_on_page)
-    # # find all the relevant elements in the page and join them into a single string
-    # text_on_page = "\n".join([tag.get_text() for tag in soup.find_all(relevant_tags)])
+    except Exception as e:
+        return jsonify({"success": False, "error": str(e)})
 
-    # create a response containing the scraped text
-    return text_on_page
 
-if __name__ == '__main__':
+# @app.route("/scrape", methods=["POST"])
+# def scrape():
+#         # get the URL from the request arguments
+#         # url = request.args.get('url')
+
+#         website_url = request.json["url"]
+
+#         # print(website_url)
+
+#         response = requests.get(website_url)
+#         # yc_web_page = response.text
+
+#         # make a request to the URL and get the content
+#         # response = requests.get(url)
+#         soup = BeautifulSoup(response.text, "html.parser")
+#         print(soup)
+#         # soup = BeautifulSoup(response.content, 'html.parser')
+
+#         # define the relevant tags to scrape
+#         relevant_tags = [
+#             "div",
+#             "h1",
+#             "h2",
+#             "h3",
+#             "h4",
+#             "h5",
+#             "h6",
+#             "p",
+#             "a",
+#             "span",
+#             "em",
+#             "strong",
+#             "b",
+#             "i",
+#             "u",
+#             "ol",
+#             "ul",
+#             "li",
+#             "img",
+#             "table",
+#             "thead",
+#             "tbody",
+#             "tfoot",
+#             "tr",
+#             "th",
+#             "td",
+#         ]
+
+#         text_on_page = ""
+#         for tag in soup.find_all(relevant_tags):
+#             if tag.string:
+#                 text_on_page += tag.string + " "
+
+#         print(text_on_page)
+#         # print(text_on_page)
+#         # # find all the relevant elements in the page and join them into a single string
+#         # text_on_page = "\n".join([tag.get_text() for tag in soup.find_all(relevant_tags)])
+
+#         # response = {
+#         #     'success': True,
+#         #     'content': text_on_page
+#         # }
+
+#         # create a response containing the scraped text
+#         return text_on_page
+
+
+if __name__ == "__main__":
     app.run(debug=True)
